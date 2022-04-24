ad94bf421d4dd67fddeb55abb854a36c5d2e265f
Howard <howard_bb@hotmail.com>
2022-04-24 20:31:05 +0800

    update: update to 3.0
    add convert pinyin, simplified, You, Him, default presets

---
+++ debug/js script reference/pinyin_without_sound.js
@@ -0,0 +1,22 @@
+
+
+function transs(){
+	var cc=document.form2.code3.value; // the field to convert
+	var str='';
+	var s;
+	for(var i=0;i<cc.length;i++){
+	//alert(cc.charAt(i)+" = "+cc.charCodeAt(i));
+		if(pydis.indexOf(cc.charAt(i))!=-1&&cc.charCodeAt(i)>200){
+			s=1;
+			while(pydis.charAt(pydis.indexOf(cc.charAt(i))+s)!=","){
+				str+=pydis.charAt(pydis.indexOf(cc.charAt(i))+s);
+				s++;
+			}
+			str+=" ";
+		}
+		else{
+			str+=cc.charAt(i);
+		}
+	}
+	return str;
+}
